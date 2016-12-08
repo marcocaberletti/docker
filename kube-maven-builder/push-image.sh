6fb7f7959cf334db82049c52c68c558018bd4b06
Marco Caberletti <marco.caberletti@gmail.com>
2016-12-08 17:56:33 +0100

    Add Kubernetes Pods builder images

---
+++ kube-maven-builder/push-image.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -xe
+
+DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm181.cloud.cnaf.infn.it"}
+BRANCH=${BRANCH:-"latest"}
+
+image_name=italiangrid/kube-maven-builder:$BRANCH
+dest=${DOCKER_REGISTRY_HOST}/$image_name
+	
+docker tag $image_name $dest
+docker push $dest
