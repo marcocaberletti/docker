c8f82aa80af45d653ba95c6ba336e5698f589e84
Marco Caberletti <marco.caberletti@gmail.com>
2016-12-13 13:35:37 +0100

    Rename from builder to runner.

--- kube-docker-runner/push-image.sh
+++ kube-docker-runner/push-image.sh
@@ -5,7 +5,7 @@ set -xe
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm181.cloud.cnaf.infn.it"}
 BRANCH=${BRANCH:-"latest"}
 
-image_name=italiangrid/kube-maven-builder:$BRANCH
+image_name=italiangrid/kube-docker-runner:$BRANCH
 dest=${DOCKER_REGISTRY_HOST}/$image_name
 	
 docker tag $image_name $dest
--- kube-maven-runner/push-image.sh
+++ kube-maven-runner/push-image.sh
@@ -5,7 +5,7 @@ set -xe
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm181.cloud.cnaf.infn.it"}
 BRANCH=${BRANCH:-"latest"}
 
-image_name=italiangrid/kube-docker-builder:$BRANCH
+image_name=italiangrid/kube-maven-runner:$BRANCH
 dest=${DOCKER_REGISTRY_HOST}/$image_name
 	
 docker tag $image_name $dest
