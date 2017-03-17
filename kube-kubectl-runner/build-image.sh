eaeea14cdf7629eefd27d7c28ea530a4e8ada945
Marco Caberletti <marco.caberletti@gmail.com>
2017-03-17 10:47:05 +0100

    Fix build argument

--- kube-kubectl-runner/build-image.sh
+++ kube-kubectl-runner/build-image.sh
@@ -5,4 +5,4 @@ set -xe
 TAG=${TAG:-"1.4.6"}
 JNLP_VERSION=${JNLP_VERSION:-"2.62"}
 
-docker build --build-arg KUBE_VERSION=${TAG},VERSION=${JNLP_VERSION} --no-cache -t italiangrid/kube-kubectl-runner:$TAG .
+docker build --build-arg KUBE_VERSION=${TAG} --build-arg VERSION=${JNLP_VERSION} --no-cache -t italiangrid/kube-kubectl-runner:$TAG .
