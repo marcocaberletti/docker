5d774a4afeb13ef8fcded1791c0aebc624e22019
Marco Caberletti <marco.caberletti@gmail.com>
2017-08-04 09:01:34 +0200

    Add egi-igtf-cas Docker image

---
+++ egi-igtf-cas/push-image.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -xe
+
+DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm114.cloud.cnaf.infn.it"}
+TAG=${TAG:-"latest"}
+
+image_name=italiangrid/egi-igtf-cas:${TAG}
+dest=${DOCKER_REGISTRY_HOST}/$image_name
+	
+docker tag $image_name $dest
+docker push $dest
